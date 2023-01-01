43bf4898563d2bcf919abbe50b4577875337019d
gary <121616196+GaryThisSidee@users.noreply.github.com>
2023-01-01 17:55:21 +0530

    added the game loop , a error handler for the parse and added color to the outputs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use std::io::{self, stdin};
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::*;
 fn main() {
     println!("Guess the number");
     
     let secret_number=rand::thread_rng().gen_range(1..100);
-    println!("Please input ur number");
-    let mut guess=String::new();
-  
-    let stdin=io::stdin();
-    stdin.read_line(&mut guess);
-    //    .expect("Failedc to read the line");
-    let guess:u32=guess.trim().parse().unwrap();
-    println!("You guessed:{}",guess);
-    println!("Number u had to guess {}",secret_number);
-    match guess.cmp(&secret_number){
-        Ordering::Equal=>println!("You win"),
-        Ordering::Greater=>println!("Too big"),
-        Ordering::Less=>println!("Too small"),
+    
+    
+    loop{
+        println!("Please input ur number");
+        let mut guess=String::new();
+        let stdin=io::stdin();
+        stdin.read_line(&mut guess);
+        let  guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input! Please try again.");
+                continue;
+            },
+        };
+        match guess.cmp(&secret_number){
+            Ordering::Equal=>
+            {
+                println!("{}","You win".green());
+                break;
+            },
+            Ordering::Greater=>println!("{}","Too big".red()),
+            Ordering::Less=>println!("{}","Too small".red()),
+        }
     }
 }
